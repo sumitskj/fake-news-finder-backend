b44f3c42429ce6dbbf816765390125cf088ebfab
sumit.jangir <sumit.jangir@globallogic.com>
2020-09-17 05:08:42 +0530

    Initial spring boot starter project setup

---
+++ src/main/java/tech/youvsyou/fakenewsfinderbackend/FakeNewsFinderBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.youvsyou.fakenewsfinderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeNewsFinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeNewsFinderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/youvsyou/fakenewsfinderbackend/FakeNewsFinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.youvsyou.fakenewsfinderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeNewsFinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
